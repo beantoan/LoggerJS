b9de480e93597094ab89a1e29a7817f25990c6b9
Đậu Quốc Toản <beantoan@gmail.com>
2019-03-19 18:10:49 +0100

    Update new version of Logger. Parse and print object value

--- logger.js
+++ logger.js
@@ -1,11 +1,12 @@
+
 var Logger = (function () {
     function Logger() {
     }
-
-    Logger.printLog = function (type, msg) {
-        switch (type) {
+    Logger.printLog = function (logType, msg, style) {
+        if (style === void 0) { style = null; }
+        switch (logType) {
             case this.INFO:
-                console.log(msg);
+                console.log("%c" + msg, style);
                 break;
             case this.WARN:
                 console.warn(msg);
@@ -15,14 +16,60 @@ var Logger = (function () {
                 break;
         }
     };
-    Logger.log = function (type, className, methodName, params) {
-        this.printLog(type, ">>> " + className + "#" + methodName + "()");
-
-        if (params && params.length > 0) {
-            for (var _i = 0; _i < params.length; _i++) {
-                this.printLog(type, params[_i]);
+    Logger.objectToStr = function (parentObjectType, objectName, objectValue) {
+        if (objectValue) {
+            if (typeof (objectValue) === 'object') {
+                var objectStr = Object.keys(objectValue).map(function (key) { return key + '=' + objectValue[key]; }).join(', ');
+                if (parentObjectType === 'Array') {
+                    return objectStr;
+                }
+                return objectName + '(' + objectStr + ')';
             }
-            this.printLog(type, 'END <<<');
+            else {
+                return objectName + "=" + objectValue;
+            }
+        }
+        return null;
+    };
+    Logger.parseObject = function (object, count) {
+        if (count === void 0) { count = 0; }
+        count = count + 1;
+        if (count > 4) {
+            return '...';
+        }
+        if (object instanceof Object) {
+            var objectName = object.constructor.name;
+            var paramArr = Object.keys(object).map(function (key) { return [key, object[key]]; });
+            var paramStr = null;
+            if (paramArr.length === 1) {
+                paramStr = this.objectToStr(objectName, paramArr[0][0], paramArr[0][1]);
+            }
+            else {
+                var data = [];
+                for (var _i = 0, paramArr_1 = paramArr; _i < paramArr_1.length; _i++) {
+                    var p = paramArr_1[_i];
+                    var objectStr = p[1] ? this.parseObject(p[1], count) : null;
+                    data.push(objectName === 'Array' ? objectStr : p[0] + '=' + objectStr);
+                }
+                paramStr = data.join(', ');
+            }
+            return objectName + '(' + paramStr + ')';
+        }
+        else {
+            return object;
+        }
+    };
+    Logger.log = function (logType, className, methodName, params) {
+        if (params && params.length > 0) {
+            this.printLog(logType, ">>> " + className + "#" + methodName + "() at " + Date(), 'background: #222; color: #fff');
+            for (var _i = 0, params_1 = params; _i < params_1.length; _i++) {
+                var param = params_1[_i];
+                this.printLog(logType, this.parseObject(param, 0));
+            }
+            this.printLog(logType, 'END <<<', 'background: #222; color: #fff');
+        }
+        else {
+            this.printLog(logType, ">>> " + className + "#" + methodName + "() at " + Date() + " END <<<", 'background: #222; color: #fff');
         }
     };
     Logger.info = function (className, methodName) {
@@ -30,7 +77,6 @@ var Logger = (function () {
         for (var _i = 2; _i < arguments.length; _i++) {
             params[_i - 2] = arguments[_i];
         }
-
         this.log(this.INFO, className, methodName, params);
     };
     Logger.error = function (className, methodName) {
--- logger.ts
+++ logger.ts
@@ -1,15 +1,12 @@
-import {Injectable} from '@angular/core';
-
-@Injectable()
 export class Logger {
   static INFO = 1;
   static WARN = 2;
   static ERROR = 3;
 
-  static printLog(type: number, msg: any) {
-    switch (type) {
+  private static printLog(logType: number, msg: any, style: string = null) {
+    switch (logType) {
       case this.INFO:
-        console.log(msg);
+        console.log(`%c${msg}`, style);
         break;
       case this.WARN:
         console.warn(msg);
@@ -20,26 +17,81 @@ export class Logger {
     }
   }
 
-  static log<T>(type: number, className: string, methodName: string, params: T[]) {
-    this.printLog(type, `>>> ${className}#${methodName}()`);
+  private static objectToStr(parentObjectType, objectName, objectValue) {
+    if (objectValue) {
+      if (typeof (objectValue) === 'object') {
+        const objectStr = Object.keys(objectValue).map((key) => key + '=' + objectValue[key]).join(', ');
 
-    if (params && params.length > 0) {
-      for (const param of params) {
-        this.printLog(type, param);
+        if (parentObjectType === 'Array') {
+          return objectStr;
+        }
+
+        return objectName + '(' + objectStr + ')';
+      } else {
+        return `${objectName}=${objectValue}`;
       }
-      this.printLog(type, 'END <<<');
+    }
+
+    return null;
+  }
+
+  private static parseObject(object, count = 0) {
+    count = count + 1;
+
+    if (count > 4) {
+      return '...';
+    }
+
+    if (object instanceof Object) {
+      const objectName = object.constructor.name;
+
+      const paramArr = Object.keys(object).map((key) => [key, object[key]]);
+
+      let paramStr = null;
+
+      if (paramArr.length === 1) {
+        paramStr = this.objectToStr(objectName, paramArr[0][0], paramArr[0][1]);
+      } else {
+        const data = [];
+
+        for (const p of paramArr) {
+          const objectStr = p[1] ? this.parseObject(p[1], count) : null;
+
+          data.push(objectName === 'Array' ? objectStr : p[0] + '=' + objectStr);
+        }
+
+        paramStr = data.join(', ');
+      }
+
+      return objectName + '(' + paramStr + ')';
+    } else {
+      return object;
     }
   }
 
-  static info<T>(className: string, methodName: string, ...params: T[]) {
+  static log(logType: number, className: string, methodName: string, params: any[]) {
+    if (params && params.length > 0) {
+      this.printLog(logType, `>>> ${className}#${methodName}() at ${Date()}`, 'background: #222; color: #fff');
+
+      for (const param of params) {
+        this.printLog(logType, this.parseObject(param, 0));
+      }
+
+      this.printLog(logType, 'END <<<', 'background: #222; color: #fff');
+    } else {
+      this.printLog(logType, `>>> ${className}#${methodName}() at ${Date()} END <<<`, 'background: #222; color: #fff');
+    }
+  }
+
+  static info(className: string, methodName: string, ...params: any[]) {
     this.log(this.INFO, className, methodName, params);
   }
 
-  static error<T>(className: string, methodName: string, ...params: T[]) {
+  static error(className: string, methodName: string, ...params: any[]) {
     this.log(this.ERROR, className, methodName, params);
   }
 
-  static warn<T>(className: string, methodName: string, ...params: T[]) {
+  static warn(className: string, methodName: string, ...params: any[]) {
     this.log(this.WARN, className, methodName, params);
   }
 }
